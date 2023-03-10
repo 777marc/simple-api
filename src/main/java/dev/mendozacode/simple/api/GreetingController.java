d30a75c927dc39710bce8972fed1cacf6f6f29c6
Marc Mendoza <777marc@gmail.com>
2023-03-10 16:46:48 -0500

    Initial commit

---
+++ src/main/java/dev/mendozacode/simple/api/GreetingController.java
@@ -0,0 +1,13 @@
+package dev.mendozacode.simple.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String getGreeting() {
+        return "Heyo Marc!";
+    }
+}
---
+++ src/test/java/dev/mendozacode/simple/api/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mendozacode.simple.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
